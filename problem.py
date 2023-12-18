fixing problem of overflow

problem.py
@@ -64,43 +64,33 @@ class LPProblem:
         s.t. Ax = b
         x >= 0
         """
+
+        inf = 1e300
+        
         m, n0 = self.A.shape
+        print(np.inf > 1e300)
+        index1 = np.where((self.lo < -inf) & (self.hi > inf))[0] + 1
+        index2 = np.where((self.lo >= -inf) & (self.hi > inf))[0] + 1
+        index3 = np.where((self.lo < -inf) & (self.hi <= inf))[0] + 1
+        index4 = np.where((self.lo >= -inf) & (self.hi <= inf))[0] + 1
 
-        index1 = np.zeros(0, dtype=int)
-        index2 = np.zeros(0, dtype=int)
-        index3 = np.zeros(0, dtype=int)
-        index4 = np.zeros(0, dtype=int)
+        n = np.array([len(index1), len(index2), len(index3), len(index4)])
+
+        cs = np.concatenate([np.take(self.c, index1-1), -np.take(self.c, index1-1),
+                             np.take(self.c, index2-1), -np.take(self.c, index3-1),
+                             np.take(self.c, index4-1), np.zeros(n[3])])
+
+        As = np.vstack([
+            np.hstack([np.take(self.A, index1-1, axis=1), -np.take(self.A, index1-1, axis=1),
+                       np.take(self.A, index2-1, axis=1), -np.take(self.A, index3-1, axis=1),
+                       np.take(self.A, index4-1, axis=1), np.zeros((m, n[3]))]),
+            np.hstack([np.zeros((n[3], 2*sum(n[:3]))), np.eye(n[3]), np.eye(n[3])])
+        ])
         
-        n = np.zeros(4, dtype=int)
-        
-        for i in range(1, n0 + 1):
-            if self.lo[i - 1] <= -np.inf:
-                if self.hi[i - 1] >= np.inf:
-                    n[0] += 1
-                    index1 = np.append(index1, i)
-                else:
-                    n[2] += 1
-                    index3 = np.append(index3, i)
-            else:
-                print(self.hi[i - 1], np.inf)
-                if self.hi[i - 1] >= np.inf:
-                    n[1] += 1
-                    index2 = np.append(index2, i)
-                else:
-                    n[3] += 1
-                    index4 = np.append(index4, i)
-                    
-        cs = np.concatenate((self.c[index1 - 1], -self.c[index1 - 1], self.c[index2 - 1],
-                            -self.c[index3 - 1], self.c[index4 - 1], np.zeros(n[3])))
-
-        print(index1, self.A[:, index2 - 1])
-        As = np.concatenate((self.A[:, index1 - 1], -self.A[:, index1 - 1], self.A[:, index2 - 1], 
-                            -self.A[:, index3 - 1], self.A[:, index4 - 1]), axis=0)
-        As = np.concatenate((As, np.zeros((n[3], 2*n[0] + n[1] + n[2])), np.eye(n[3]), np.eye(n[3])), axis=1)
-
-        bs = np.concatenate((self.b - self.A[:, index2 - 1] @ self.lo[index2 - 1] - self.A[:, index3 - 1] @ self.hi[index3 - 1] -
-                            self.A[:, index4 - 1] @ self.lo[index4 - 1],
-                            self.hi[index4 - 1] - self.lo[index4 - 1]))
+        bs = np.concatenate([self.b - np.dot(self.A[:, index2-1], self.lo[index2-1]) -
+                             np.dot(self.A[:, index3-1], self.hi[index3-1]) -
+                             np.dot(self.A[:, index4-1], self.lo[index4-1]),
+                             self.hi[index4-1] - self.lo[index4-1]])
 
         self.A = As
         self.b = bs
@@ -123,7 +113,8 @@ class LPProblem:
         self.presolve()
         
         # TODO: convert to standard form
-        # result = self.convert_to_standard()
+        ind1, ind2, ind3, ind4 = self.convert_to_standard()
+        print(self)
         # print(result)
         # detect infeasibility
         if feasibility.is_not_feasible(self.A, self.b):
@@ -141,13 +132,12 @@ if __name__ == "__main__":
         [[-2, 1],
          [-1, 2],
          [1, 0],
-         [0, 0]
         ])
-    b = np.array([2, 7, 3, 0])
+    b = np.array([2, 7, 3])
     c = np.array([-1, -2])
     
-    lo = np.array([10, 0])
-    hi = np.array([0, 200])
+    lo = np.array([0, 0])
+    hi = np.array([5, 5])
                    
-    P = LPProblem(A, b, c, lo, hi)
-    P.internal_point(tolerance=1)
+    P = LPProblem(A, b, c)
+    P.internal_point(tolerance=1e-8)